899268cccea9083dfcbfdb4bb7675ac46d3e959d
trungtruongpham <trungtruongpham@gmail.com>
2025-01-04 18:32:23 +0700

    Remove env.local and update env variables

--- src/lib/supabase/supabase.ts
+++ src/lib/supabase/supabase.ts
@@ -1,12 +1,11 @@
 import { type Database } from "../database.types";
 import { createBrowserClient } from "@supabase/ssr";
 
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL)
-  throw new Error("Missing NEXT_PUBLIC_SUPABASE_URL");
-if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
-  throw new Error("Missing NEXT_PUBLIC_SUPABASE_ANON_KEY");
+if (!process.env.SUPABASE_URL) throw new Error("Missing SUPABASE_URL");
+if (!process.env.SUPABASE_ANON_KEY)
+  throw new Error("Missing SUPABASE_ANON_KEY");
 
 export const supabase = createBrowserClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 );
